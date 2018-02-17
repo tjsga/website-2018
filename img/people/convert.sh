7fa65b8e3f8ce43fd0b47001ec005f79869f812e
William Zhang <willzhang05@gmail.com>
2018-02-17 18:21:10 -0500

    optimize images

--- img/people/convert.sh
+++ img/people/convert.sh
@@ -3,7 +3,7 @@ FILES=.
 for f in $FILES/*.jpg
 do
     mkdir -p $FILES/out;
-    convert $f -strip -resize 350x350 -quality 85 ${f%.*}-new.jpg;
+    convert $f -sampling-factor 4:2:0 -strip -resize 350x350 -quality 85 -interlace JPEG -colorspace sRGB ${f%.*}-new.jpg;
     mv $FILES/*-new.jpg $FILES/out/$f;
 
 done
--- img/web-image.sh
+++ img/web-image.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-convert  -sampling-factor 4:2:0 -strip -quality 60 -interlace JPEG -colorspace RGB 
+convert $1 -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace sRGB $2
