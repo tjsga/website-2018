93ab09f68520876203b6e722f0e353f320a20d3d
William Zhang <willzhang05@gmail.com>
2018-05-08 22:30:06 -0400

    start templating

--- sgawebsite/apps/main/models.py
+++ sgawebsite/apps/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    intro = models.CharField(max_length=140)
+    title = models.CharField(max_length=30)
+    CATEGORIES = (('officers', 'Officers'), ('excomm', 'Executive Committee'), ('senators', 'Class Senators'), ('sponsors', 'Sponsors'))
+    category = models.CharField(max_length=1, choices=CATEGORIES)
+
+class Resource(models.Model):
+    name = models.CharField(max_length=50)
+    link = models.URLField()
+    text = models.CharField(max_length=140)
+    CATEGORIES = (('general', 'General Resources'), ('event', 'Event Resources'))
+    category = models.CharField(max_length=1, choices=CATEGORIES)
--- sgawebsite/apps/main/views.py
+++ sgawebsite/apps/main/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .models import Member, Resource
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
 def about(request):
-    return render(request, "about.html")
+    categories = Member.CATEGORIES
+    context = dict()
+    data = dict()
+    for c in categories:
+        data[c[0]] = Member.objects.filter(category=c[0])
+    context['categories'] = categories
+    context['data'] = data
+    print(context)
+    return render(request, 'about.html', context)
 
 def resources(request):
-    return render(request, "resources.html")
+    return render(request, 'resources.html')
 
 def events(request):
-    return render(request, "events.html")
+    return render(request, 'events.html')
