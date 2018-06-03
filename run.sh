8c2ea0b4505d1f79e7eac270da639129dc7cb177
willzhang05 <willzhang05@gmail.com>
2018-06-03 17:44:01 -0400

    make run.sh right

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@
 #/web/activities/ghs/public/env/bin/python run.py $PORT
 #export PATH=$PATH:/home/2018wzhang/.gem/ruby/2.3.0/bin;
 #/home/2018wzhang/.gem/ruby/2.3.0/bin/bundler exec jekyll serve --port $PORT
-/web/activities/sga/sgawebsite/manage.py runserver $PORT
+#/web/activities/sga/sgawebsite/manage.py runserver $PORT
+/web/activities/sga/sgawebsite/env/bin/python3 manage.py runserver $PORT
 #/web/activities/ghs/public/env/bin/python /web/activities/ghs/public/ghswebsite/app.py $PORT
